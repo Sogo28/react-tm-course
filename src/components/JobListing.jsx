a6425065f6af49f5a80dfa8e6e72507f56beb7be
Daniel <107406034+M0onstruck@users.noreply.github.com>
2024-06-04 17:48:28 +0000

    fetching datas + adding a loader

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,23 +1,49 @@
 import JobCard from "./JobCard";
-import jobs from "../jobs.json";
 import Section from "./Section";
+import { useState, useEffect } from "react";
+import Loading from "./Loading";
 export default function JobListing({ isHome = false }) {
-  let recentJobs = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/jobs");
+        const data = await res.json();
+        isHome ? setJobs(data.slice(0, 3)) : setJobs(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    setTimeout(() => {
+      fetchJobs();
+    }, 2000);
+  }, []);
   return (
-    <Section bgColor={"bg-slate-100"} title="Browse Job">
-      <div className="flex flex-col gap-12 xl:grid xl:grid-cols-3 ">
-        {recentJobs.map((job) => (
-          <JobCard
-            key={job.id}
-            title={job.title}
-            description={job.description}
-            mode={job.type}
-            location={job.location}
-            link={`/jobs/${job.id}`}
-            salary={job.salary}
-          ></JobCard>
-        ))}
-      </div>
+    <Section
+      bgColor={"bg-slate-100"}
+      title="Browse Job"
+      custom="flex flex-col justify-center items-center"
+    >
+      {loading ? (
+        <Loading loading={loading} />
+      ) : (
+        <div className="flex flex-col gap-12 xl:grid xl:grid-cols-3 ">
+          {jobs.map((job) => (
+            <JobCard
+              key={job.id}
+              title={job.title}
+              description={job.description}
+              mode={job.type}
+              location={job.location}
+              link={`/jobs/${job.id}`}
+              salary={job.salary}
+            ></JobCard>
+          ))}
+        </div>
+      )}
     </Section>
   );
 }
